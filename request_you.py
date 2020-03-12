0363e1266bf5dfa1b9d777b6e4d1ea16997fb8a2
JavoJavo <javojavojavojavo@gmail.com>
2020-03-11 21:18:19 -0600

    Add files via upload

---
+++ request_you.py
@@ -0,0 +1,12 @@
+api_key =[API_KEY]
+
+from apiclient.discovery import build
+
+youtube = build('youtube','v3',developerKey
+                =api_key)
+#print(type(youtube))
+
+req = youtube.search().list(q='cats',part='snippet',type='video',maxResults=1)
+res = req.execute()
+print(res['items'][0]['id']['videoId'])
+
