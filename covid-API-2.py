7a159d09f3ad38e116a9e4a4954394b85a350693
JavoJavo <javojavojavojavo@gmail.com>
2020-07-23 18:46:33 -0500

    Add files via upload

---
+++ covid-API-2.py
@@ -0,0 +1,16 @@
+import requests
+
+countries = ['united-states','brazil','mexico','colombia','argentina','canada','peru','venezuela','chile','ecuador']
+
+
+for country in countries:
+	response = requests.get('https://api.covid19api.com/total/country/{}/status/confirmed'.format(country))
+	jsonResponse = response.json()
+
+	f = open("{}_data.txt".format(country), "w")
+
+	for dia in jsonResponse:
+		record = str(dia['Date'])+' '+str(dia['Cases'])+'\n'
+		f.write(record)
+	
+	f.close()
