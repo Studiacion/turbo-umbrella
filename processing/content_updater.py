a3996144b305a9d814f3213daef5b3848bb3f7e6
FonseK1ng <60753156+FonseK1ng@users.noreply.github.com>
2020-08-09 13:21:59 -0500

    Add files via upload

--- processing/content_updater.py
+++ processing/content_updater.py
@@ -15,118 +15,166 @@ video_ID_EC = f.readline()
 
 f.close()
 
-f = open("/home/{}/public_html/static/page.html".format(username), "w")
+f = open("/home/{}/public_html/static/videos.html".format(username), "w")
 
 f.write('''
 <!DOCTYPE HTML>
+<!--
+	Phantom by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
 <html>
+	<head>
+		<title>Studiacion</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+		<!-- Wrapper -->
+			<div id="wrapper">
 
-<head>
-  <title>simplestyle_blue_trees - a page</title>
-  <meta name=\"description\" content=\"website description\" />
-  <meta name=\"keywords\" content=\"website keywords, website keywords\" />
-  <meta http-equiv=\"content-type\" content=\"text/html; charset=windows-1252\" />
-  <link rel=\"stylesheet\" type=\"text/css\" href=\"style/style.css\" />
-</head>
+				<!-- Header -->
+					<header id="header">
+						<div class="inner">
 
-<body>
-  <div id=\"main\">
-    <div id=\"header\">
-      <div id=\"logo\">
-        <div id=\"logo_text\">
-          <!-- class=\"logo_colour\", allows you to change the colour of the text -->
-          <h1><a href=\"index.html\">simple<span class=\"logo_colour\">style_blue_trees</span></a></h1>
-          <h2>Simple. Contemporary. Website Template.</h2>
-        </div>
-      </div>
-      <div id=\"menubar\">
-        <ul id=\"menu\">
-          <!-- put class=\"selected\" in the li tag for the selected page - to highlight which page you\'re on -->
-          <li><a href=\"index.html\">Home</a></li>
-          <li><a href=\"examples.html\">Examples</a></li>
-          <li class=\"selected\"><a href=\"page.html\">A Page</a></li>
-          <li><a href=\"another_page.html\">Another Page</a></li>
-          <li><a href=\"contact.html\">Contact Us</a></li>
-        </ul>
-      </div>
-    </div>
-    <div id=\"content_header\"></div>
-    <div id=\"site_content\">
-      <div id=\"sidebar_container\">
-        <div class=\"sidebar\">
-          <div class=\"sidebar_top\"></div>
-          <div class=\"sidebar_item\">
-            <!-- insert your sidebar items here -->
-            <h3>Latest News</h3>
-            <h4>New Website Launched</h4>
-            <h5>February 1st, 2014</h5>
-            <p>2014 sees the redesign of our website. Take a look around and let us know what you think.<br /><a href=\"#\">Read more</a></p>
-          </div>
-          <div class=\"sidebar_base\"></div>
-        </div>
-        <div class=\"sidebar\">
-          <div class=\"sidebar_top\"></div>
-          <div class=\"sidebar_item\">
-            <h3>Useful Links</h3>
-            <ul>
-              <li><a href=\"#\">link 1</a></li>
-              <li><a href=\"#\">link 2</a></li>
-              <li><a href=\"#\">link 3</a></li>
-              <li><a href=\"#\">link 4</a></li>
-            </ul>
-          </div>
-          <div class=\"sidebar_base\"></div>
-        </div>
-        <div class=\"sidebar\">
-          <div class=\"sidebar_top\"></div>
-          <div class=\"sidebar_item\">
-            <h3>Search</h3>
-            <form method=\"post\" action=\"#\" id=\"search_form\">
-              <p>
-                <input class=\"search\" type=\"text\" name=\"search_field\" value=\"Enter keywords.....\" />
-                <input name=\"search\" type=\"image\" style=\"border: 0; margin: 0 0 -9px 5px;\" src=\"style/search.png\" alt=\"Search\" title=\"Search\" />
-              </p>
-            </form>
-          </div>
-          <div class=\"sidebar_base\"></div>
-        </div>
-      </div>
-      <div id=\"content\">
-        <!-- insert the page content here -->
+							<!-- Logo -->
+								<a href="index.html" class="logo">
+									<span class="symbol"><img src="images/logo.png" alt="" /></span><span class="title">Studiacion</span>
+								</a>
 
+							<!-- Nav -->
+								<nav>
+									<ul>
+										<li><a href="#menu">Menu</a></li>
+									</ul>
+								</nav>
 
-        <h2 style=\"text-align: center;\">United States</h2>
-        <p style=\"text-align: center;\"><iframe allowfullscreen=\"allowfullscreen\" height=\"315\" src=\"https://www.youtube.com/embed/{}\" width=\"560\"></iframe></p>
-        <h2 style=\"text-align: center;\">Brazil</h2>
-        <p><iframe width=\"560\" height=\"315\" style=\"display: block; margin-left: auto; margin-right: auto;\" allowfullscreen=\"allowfullscreen\" src=\"https://www.youtube.com/embed/{}\"></iframe></p>
-        <h2 style=\"text-align: center;\">Mexico</h2>
-        <p><iframe width=\"560\" height=\"315\" style=\"display: block; margin-left: auto; margin-right: auto;\" allowfullscreen=\"allowfullscreen\" src=\"https://www.youtube.com/embed/{}\"></iframe></p>
-        <h2 style=\"text-align: center;\">Argentina</h2>
-        <p><iframe width=\"560\" height=\"315\" style=\"display: block; margin-left: auto; margin-right: auto;\" allowfullscreen=\"allowfullscreen\" src=\"https://www.youtube.com/embed/{}\"></iframe></p>
-        <h2 style=\"text-align: center;\">Colombia</h2>
-        <p><iframe width=\"560\" height=\"315\" style=\"display: block; margin-left: auto; margin-right: auto;\" allowfullscreen=\"allowfullscreen\" src=\"https://www.youtube.com/embed/{}\"></iframe></p>
-        <h2 style=\"text-align: center;\">Canada</h2>
-        <p><iframe width=\"560\" height=\"315\" style=\"display: block; margin-left: auto; margin-right: auto;\" allowfullscreen=\"allowfullscreen\" src=\"https://www.youtube.com/embed/{}\"></iframe></p>
-        <h2 style=\"text-align: center;\">Peru</h2>
-        <p><iframe width=\"560\" height=\"315\" style=\"display: block; margin-left: auto; margin-right: auto;\" allowfullscreen=\"allowfullscreen\" src=\"https://www.youtube.com/embed/{}\"></iframe></p>
-        <h2 style=\"text-align: center;\">Venezuela</h2>
-        <p><iframe width=\"560\" height=\"315\" style=\"display: block; margin-left: auto; margin-right: auto;\" allowfullscreen=\"allowfullscreen\" src=\"https://www.youtube.com/embed/{}\"></iframe></p>
-        <h2 style=\"text-align: center;\">Chile</h2>
-        <p><iframe width=\"560\" height=\"315\" style=\"display: block; margin-left: auto; margin-right: auto;\" allowfullscreen=\"allowfullscreen\" src=\"https://www.youtube.com/embed/{}\"></iframe></p>
-        <h2 style=\"text-align: center;\">Ecuador</h2>
-        <p><iframe width=\"560\" height=\"315\" style=\"display: block; margin-left: auto; margin-right: auto;\" allowfullscreen=\"allowfullscreen\" src=\"https://www.youtube.com/embed/{}\"></iframe></p>
+						</div>
+					</header>
 
-      </div>
-    </div>
-    <div id=\"content_footer\"></div>
-    <div id=\"footer\">
-      <p><a href=\"index.html\">Home</a> | <a href=\"examples.html\">Examples</a> | <a href=\"page.html\">A Page</a> | <a href=\"another_page.html\">Another Page</a> | <a href=\"contact.html\">Contact Us</a></p>
-      <p>Copyright &copy; simplestyle_blue_trees | <a href=\"http://validator.w3.org/check?uri=referer\">HTML5</a> | <a href=\"http://jigsaw.w3.org/css-validator/check/referer\">CSS</a> | <a href=\"http://www.html5webtemplates.co.uk\">design from HTML5webtemplates.co.uk</a></p>
-    </div>
-  </div>
-</body>
+				<!-- Menu -->
+					<nav id="menu">
+						<h2>Menu</h2>
+						<ul>
+							<li><a href="index.html">Home</a></li>
+							<li><a href="lives.html">Streamings</a></li>
+							<li><a href="videos.html">Videos</a></li>
+							<li><a href="graphics.html">Graphics</a></li>
+						</ul>
+					</nav>
+
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+							<span class="image right"><img src="images/vidios.jpg" alt="" /></span><h1>Popular Videos</h1>
+							<!-- <span class="image main"><img src="images/pic13.jpg" alt="" /></span> -->
+						</div>
+						<hr />
+
+						<div class="inner">
+							<span class="image left"><img src="images/mexico.jpg" alt="" /></span><h2 style="font-size:35px">Mexico</h2>
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/{}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						</div>
+						<hr />
+
+						<div class="inner">
+							<span class="image left"><img src="images/gringos.jpg" alt="" /></span><h2 style="font-size:35px">United States</h2>
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/{}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						</div>
+						<hr />
+
+						<div class="inner">
+							<span class="image left"><img src="images/brazil.jpg" alt="" /></span><h2 style="font-size:35px">Brazil</h2>
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/{}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						</div>
+						<hr />
+
+						<div class="inner">
+							<span class="image left"><img src="images/canada.jpg" alt="" /></span><h2 style="font-size:35px">Canada</h2>
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/{}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						</div>
+						<hr />
+
+						<div class="inner">
+							<span class="image left"><img src="images/arg.jpg" alt="" /></span><h2 style="font-size:35px">Argentina</h2>
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/{}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						</div>
+						<hr />
+
+						<div class="inner">
+							<span class="image left"><img src="images/chile.jpg" alt="" /></span><h2 style="font-size:35px">Chile</h2>
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/{}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						</div>
+						<hr />
+
+						<div class="inner">
+							<span class="image left"><img src="images/colombia.jpg" alt="" /></span><h2 style="font-size:35px">Colombia</h2>
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/{}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						</div>
+						<hr />
+
+						<div class="inner">
+							<span class="image left"><img src="images/ecuador.jpg" alt="" /></span><h2 style="font-size:35px">Ecuador</h2>
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/{}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						</div>
+						<hr />
+
+						<div class="inner">
+							<span class="image left"><img src="images/peru.jpg" alt="" /></span><h2 style="font-size:35px">Peru</h2>
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/{}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						</div>
+						<hr />
+
+						<div class="inner">
+							<span class="image left"><img src="images/venezuela.jpg" alt="" /></span><h2 style="font-size:34px">Venezuela</h2>
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/{}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						</div>
+
+					</div>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<div class="inner">
+							<section>
+								<h2>Get in touch</h2>
+								<form method="post" action="#">
+									<div class="fields">
+										<div class="field half">
+											<input type="text" name="name" id="name" placeholder="Name" />
+										</div>
+										<div class="field half">
+											<input type="email" name="email" id="email" placeholder="Email" />
+										</div>
+										<div class="field">
+											<textarea name="message" id="message" placeholder="Message"></textarea>
+										</div>
+									</div>
+									<ul class="actions">
+										<li><input type="submit" value="Send" class="primary" /></li>
+									</ul>
+								</form>
+							</section>
+							<ul class="copyright">
+								<li>&copy; Untitled. All rights reserved</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+							</ul>
+						</div>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
 </html>
-'''.format(video_ID_US, video_ID_BR, video_ID_MX, video_ID_AR, video_ID_CO, video_ID_CA, video_ID_PE, video_ID_VE, video_ID_CL,  video_ID_EC))
+'''.format(video_ID_MX, video_ID_US, video_ID_BR, video_ID_CA, video_ID_AR, video_ID_CL, video_ID_CO, video_ID_EC, video_ID_PE, video_ID_VE))
 
 
 
